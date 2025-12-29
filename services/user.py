93cbb79fc55bbc3c935612e8a4c64c91a841f9af
hector <h.rosales01@ufromail.cl>
2025-12-29 18:27:55 -0300

    feat: implement user creation service with email validation

---
+++ services/user.py
@@ -0,0 +1,24 @@
+from sqlmodel import Session, select
+from models.models import User
+from exceptions.excep import EmailAlreadyExists, DatabaseError
+from schemas.user import UserResponse, UserCreate
+
+def create_user_service(user_data: UserCreate, db_session: Session) -> UserResponse:
+
+    if get_user_by_email(user_data.email, db_session):
+        raise EmailAlreadyExists(user_data.email)
+
+    try:
+        new_user = User(email=user_data.email, username=user_data.username)
+        db_session.add(new_user)
+        db_session.commit() 
+        db_session.refresh(new_user)
+        return UserResponse(email=new_user.email, created=True)
+    
+    except Exception as e:
+        db_session.rollback()
+        raise DatabaseError()
+    
+def get_user_by_email(email: str, db_session: Session) -> User | None:
+    statement = select(User).where(User.email == email)
+    return db_session.exec(statement).first()
