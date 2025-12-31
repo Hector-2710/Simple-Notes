5aba6c14fd5e370cfbeb45edad5676b058d06a08
hector <h.rosales01@ufromail.cl>
2025-12-30 22:52:50 -0300

    refactor: update user router to include detailed response descriptions for user creation

--- api/routes/user.py
+++ api/routes/user.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter
 from db.session import SessionDep
 from services.user import create_user_service 
 from schemas.user import UserResponse, UserCreate
+from exceptions.responses import EmailAlreadyExists, DatabaseError
 
-router = APIRouter()
+user = APIRouter(prefix="/users", tags=["Users"])
 
-@router.post("/users")
+@user.post("/", description="Create a new user",response_description="User created successfully", responses={409: {"model": EmailAlreadyExists,"description": "User with this email already exists"}, 500: {"model": DatabaseError,"description": "A database error occurred"}},summary="Create User",status_code=201)
 def create_user(user: UserCreate, db_session : SessionDep) -> UserResponse:
     created_user = create_user_service(user, db_session)
     return created_user
