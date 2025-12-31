5b06fee43c65a8d8804d590ec84d4abb5581f220
hector <h.rosales01@ufromail.cl>
2025-12-30 22:48:32 -0300

    feat: include user and note routers

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from db.session import create_db_and_tables
-from api.routes.user import router 
+from api.routes.user import user
+from api.routes.note import note
 from exceptions.excep import register_exception_handlers
 
 @asynccontextmanager
@@ -9,6 +10,8 @@ async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
-app = FastAPI(lifespan=lifespan)
-app.include_router(router)
+app = FastAPI(lifespan=lifespan,summary="Simple API for note management whit db in PostgreSQL",version="1.0.0",title="Simple-Notes",)
+app.include_router(user)
+app.include_router(note)
 register_exception_handlers(app)
+    
