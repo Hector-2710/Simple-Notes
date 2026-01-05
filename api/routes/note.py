cdc19c27c8af39903e026a8d4c6c39feab874e44
hector <h.rosales01@ufromail.cl>
2026-01-05 01:36:42 -0300

    feat: add get_notes_for_user endpoint to retrieve notes for a user

--- api/routes/note.py
+++ api/routes/note.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from schemas.note import NoteCreate, NoteResponse
 from db.session import SessionDep
 from services.note import create_note_for_user_service
+from services.note import get_notes_for_user_service
 
 note = APIRouter(prefix="/notes", tags=["notes"])  
 
@@ -9,3 +10,8 @@ note = APIRouter(prefix="/notes", tags=["notes"])
 def create_note(user_id: int, note: NoteCreate, session: SessionDep) -> NoteResponse:
     created_note = create_note_for_user_service(user_id, note, session)
     return created_note
+
+@note.get("/{user_id}")
+def get_notes_for_user(user_id: int, session: SessionDep) -> list[NoteResponse]:
+    notes = get_notes_for_user_service(user_id, session)
+    return notes
