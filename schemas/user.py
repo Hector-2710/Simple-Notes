6be0e24fc1171ee5f58332bdfe1aea017004e9ca
hector <h.rosales01@ufromail.cl>
2026-01-05 13:36:00 -0300

    feat: add password field to UserLogin and UserCreate models

--- schemas/user.py
+++ schemas/user.py
@@ -3,8 +3,12 @@ from pydantic import BaseModel, EmailStr, Field
 class UserCreate(BaseModel):
     email: EmailStr
     username: str = Field(min_length=3, max_length=20)
+    password: str = Field(min_length=6)
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str = Field(min_length=6)
 
 class UserResponse(BaseModel):
     id: int
     email: EmailStr
-    created : bool 
