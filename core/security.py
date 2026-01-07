39b4fcb90e5f03c60bba77a962164bedd604890d
hector <h.rosales01@ufromail.cl>
2026-01-06 23:33:42 -0300

    feat: add security module for password hashing and verification using bcrypt

---
+++ core/security.py
@@ -0,0 +1,15 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(
+    schemes=["bcrypt"], 
+    deprecated="auto",
+    bcrypt__ident="2b"  
+)
+
+def password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
