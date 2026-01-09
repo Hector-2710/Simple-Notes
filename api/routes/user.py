afc3f0ed7c7eb00a2037891a9c5d524bfa42d07c
hector <h.rosales01@ufromail.cl>
2026-01-09 20:23:41 -0300

    refactor: update login_user function to use Annotated for dependency injection

--- api/routes/user.py
+++ api/routes/user.py
@@ -5,7 +5,7 @@ from services.user import create_user_service , login_service
 from schemas.user import UserResponse, UserCreate
 from schemas.token import Token
 from exceptions.responses import EmailAlreadyExists, DatabaseError, invalidCredentials
-
+from typing import Annotated
 user = APIRouter(prefix="/users", tags=["Users"])
 
 @user.post("/", description="Create a new user",response_description="User created successfully", responses={409: {"model": EmailAlreadyExists,"description": "User with this email already exists"}, 500: {"model": DatabaseError,"description": "A database error occurred"}},summary="Create User",status_code=201)
@@ -14,5 +14,5 @@ def create_user(user: UserCreate, db_session : SessionDep) -> UserResponse:
     return created_user
 
 @user.post("/login", description="Login a user",response_model=Token,response_description="User logged in successfully", responses={401: {"model": invalidCredentials,"description": "Invalid email or password"}},summary="Login User",status_code=200)
-def login_user(db_session : SessionDep, form_data: OAuth2PasswordRequestForm = Depends()) -> Token:
+def login_user(db_session : SessionDep, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
     return login_service(form_data, db_session)
