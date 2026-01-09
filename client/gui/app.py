e90c083562f1e77c41124893fc352ab9d487aaf7
hector <h.rosales01@ufromail.cl>
2026-01-09 15:23:01 -0300

    refactor: enhance layout by adjusting packing options in SimpleNotesApp

--- client/gui/app.py
+++ client/gui/app.py
@@ -9,24 +9,23 @@ class SimpleNotesApp(tk.Tk):
         self.user_token = None
         self.geometry("600x700")
         self.container = tk.Frame(self)
-        self.container.pack(expand=True, fill="both")
-        
+        self.container.pack()
+    
         self.show_auth()
 
     def show_auth(self):
         self._clear_screen()
         auth = AuthFrame(self.container, on_success=self.login_success)
-        auth.pack()
+        auth.pack(expand=True, fill="both")
 
     def login_success(self, data):
         self.user_token = data.get("access_token")
-        print("Token almacenado en la app:", self.user_token)
         self.show_notes() 
 
     def show_notes(self):
-        self._clear_screen()
+        self._clear_screen() 
         notes_frame = NotesFrame(self.container, self.user_token)
-        notes_frame.pack(fill="both", expand=True)
+        notes_frame.pack(expand=True, fill="both")
 
     def _clear_screen(self):
         for widget in self.container.winfo_children():
