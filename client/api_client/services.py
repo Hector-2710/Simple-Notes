bcf16cfa70af6894a5e7a29482fac6129e5d9a70
hector <h.rosales01@ufromail.cl>
2026-01-09 01:20:59 -0300

    refactor: update save_note_api to use token for authorization and clean up code

--- client/api_client/services.py
+++ client/api_client/services.py
@@ -7,10 +7,18 @@ def register_user_api(username, email, password):
     return requests.post(f"{BASE_URL}/users", json=payload)
 
 def login_user_api(email, password):
-    # Usamos data= porque FastAPI OAuth2 espera formulario, no JSON
     form_data = {'username': email, 'password': password}
     return requests.post(f"{BASE_URL}/users/login", data=form_data)
 
-def save_note_api(user_id, title, content):
+def save_note_api(token, title, content):
     payload = {"title": title, "content": content}
-    return requests.post(f"{BASE_URL}/notes/{user_id}", json=payload)
+    headers = {"Authorization": f"Bearer {token}"}
+    return requests.post(f"{BASE_URL}/notes/", json=payload, headers=headers)
+
+def get_notes_api(token):
+    headers = {"Authorization": f"Bearer {token}"}
+    return requests.get(f"{BASE_URL}/notes/", headers=headers)
+
+def delete_note_api(token, title):
+    headers = {"Authorization": f"Bearer {token}"}
+    return requests.delete(f"{BASE_URL}/notes/{title}", headers=headers)
