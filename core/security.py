651ba544f10b8ee2226fa12dbe3b8b49f2d3e44d
hector <h.rosales01@ufromail.cl>
2026-01-09 22:25:20 -0300

    refactor: Update to libraries recommended by FastAPI

--- core/security.py
+++ core/security.py
@@ -1,21 +1,17 @@
+import jwt  
 from datetime import datetime, timedelta, timezone
 from typing import Dict, Any, Annotated
-
-import jwt  # Ahora usa PyJWT
 from jwt.exceptions import InvalidTokenError
-from pwdlib import PasswordHash # Nueva recomendación para hashing
-from fastapi import Depends, HTTPException, status
+from pwdlib import PasswordHash 
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
-
 from core.settings import settings
 from db.session import SessionDep
 from schemas.token import TokenData
 from models.models import User
 from exceptions.excep import InvalidToken, UserNotExists
 
-# Configuración de hashing (usa Argon2id por defecto, que es más seguro que bcrypt)
 password_hash_context = PasswordHash.recommended()
-
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
 def password_hash(password: str) -> str:
@@ -29,7 +25,6 @@ def create_access_token(data: Dict[str, Any]) -> str:
     now = datetime.now(timezone.utc)
     expire = now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     
-    # PyJWT maneja objetos datetime directamente para la clave 'exp'
     to_encode.update({"exp": expire, "iat": now})
     
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
@@ -40,10 +35,9 @@ def decode_token(token: str) -> TokenData:
         payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         sub: str | None = payload.get("sub")
         if sub is None:
-            # Reutilizamos tu excepción personalizada
             raise InvalidToken()
         return TokenData(sub=sub)
-    except InvalidTokenError: # Cambio de JWTError a InvalidTokenError
+    except InvalidTokenError: 
         raise InvalidToken() 
 
 def get_current_user(db_session: SessionDep, token: Annotated[str, Depends(oauth2_scheme)]) -> User:
@@ -56,5 +50,4 @@ def get_current_user(db_session: SessionDep, token: Annotated[str, Depends(oauth
         raise UserNotExists(email=token_data.sub)
     return user
 
-# Alias para usar en los Endpoints
 GetCurrentUser = Annotated[User, Depends(get_current_user)]
